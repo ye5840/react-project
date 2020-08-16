eed194378292f8a404c5a977c58a1ba33ff99f6c
linzhen5840 <584029578@qq.com>
2020-08-16 23:49:19 +0800

    8.16

--- src/config/asyncComps.js
+++ src/config/asyncComps.js
@@ -19,8 +19,10 @@ const Chapter = () => lazy(() => import("@pages/Edu/Chapter"));
 const Comment = () => lazy(() => import("@pages/Edu/Comment"));
 const Course = () => lazy(() => import("@pages/Edu/Course"));
 const Teacher = () => lazy(() => import("@pages/Edu/Teacher"));
+const Subject = () => lazy(() => import("@pages/Edu/Subject"));
 const Settings = () => lazy(() => import("@pages/User/Settings"));
 const Center = () => lazy(() => import("@pages/User/Center"));
+const Test = () => lazy(() => import("@pages/Edu/Test"));
 
 export default {
   Admin,
@@ -37,4 +39,6 @@ export default {
   Teacher,
   Settings,
   Center,
+  Test,
+  Subject
 };
---
+++ src/pages/Edu/Test/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        Test组件
+      </div>
+    )
+  }
+}
+export default Test
